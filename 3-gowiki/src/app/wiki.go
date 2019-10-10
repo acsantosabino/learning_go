3369baf28f02a17020b27fa6220bb45a1e844a80
Arthur Caetano Sabino Santos <arthur.santos@gjccorp.com.br>
2019-10-10 19:12:22 -0300

    Web App final tasks
    * Store templates in tmpl/ and page data in data/.
    * Add a handler to make the web root redirect to /view/FrontPage.
    * Implement inter-page linking by converting instances of [PageName] to <a href="/view/PageName">PageName</a>. (hint: you could use regexp.ReplaceAllFunc to do this

--- 3-gowiki/src/app/wiki.go
+++ 3-gowiki/src/app/wiki.go
@@ -5,17 +5,30 @@ import (
     "regexp"
 	"log"
     "net/http"
-	"html/template"
+    "io"
+    "html/template"
+    "bytes"
 )
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+var validPath = regexp.MustCompile("^/(edit|save|view)?/?([a-zA-Z0-9]+)$")
+var pageList = regexp.MustCompile("(\\[PagesList\\])")
+var fileName = regexp.MustCompile("^([a-zA-Z0-9]+).txt$")
+var filepath = "../tmpl/"
+var templates = template.Must(template.ParseFiles(filepath+"edit.html", filepath+"view.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *page.Page) {
-    err := templates.ExecuteTemplate(w, tmpl+".html", p)
+    html := new(bytes.Buffer)
+	err := templates.ExecuteTemplate(html, tmpl + ".html", p)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
+    finalResponse := string(pageList.ReplaceAllFunc(html.Bytes(),func(s []byte) []byte{
+        return interPages()
+    }))
+	_, err = io.WriteString(w, finalResponse)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -46,6 +59,10 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
     http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request, title string) {
+    http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         m := validPath.FindStringSubmatch(r.URL.Path)
@@ -57,7 +74,22 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
     }
 }
 
+func interPages() []byte{
+    pages, _ := page.AllPages()
+    var list string
+
+    for i := 0; i < len(pages); i++ {
+        m := fileName.FindStringSubmatch(pages[i])
+        if m != nil {
+            list += "<li><a href=\"/view/"+m[1]+"\">"+m[1]+"</a></li>"
+        }
+    }
+    return []byte(list)
+}
+
 func main() {
+    page.CheckPagesPath()
+    http.HandleFunc("/", makeHandler(rootHandler))
     http.HandleFunc("/view/", makeHandler(viewHandler))
     http.HandleFunc("/edit/", makeHandler(editHandler))
     http.HandleFunc("/save/", makeHandler(saveHandler))
