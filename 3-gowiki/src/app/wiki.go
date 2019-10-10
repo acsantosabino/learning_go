814710e5c932353bc0b5a076d3f4ff648a252f31
Arthur Caetano Sabino Santos <arthur.santos@gjccorp.com.br>
2019-10-10 15:37:30 -0300

    Clear code

--- 3-gowiki/src/app/wiki.go
+++ 3-gowiki/src/app/wiki.go
@@ -3,7 +3,6 @@ package main
 import (
     "page"
     "regexp"
-    // "errors"
 	"log"
     "net/http"
 	"html/template"
@@ -11,15 +10,6 @@ import (
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-// func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
-//     m := validPath.FindStringSubmatch(r.URL.Path)
-//     if m == nil {
-//         http.NotFound(w, r)
-//         return "", errors.New("Invalid Page Title")
-//     }
-//     return m[2], nil // The title is the second subexpression.
-// }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *page.Page) {
     err := templates.ExecuteTemplate(w, tmpl+".html", p)
