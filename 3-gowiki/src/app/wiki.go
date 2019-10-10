b62a67629003cba8830133a5c9d27b96cb8a6af9
Arthur Caetano Sabino Santos <arthur.santos@gjccorp.com.br>
2019-10-10 15:12:00 -0300

    Optimize templates

--- 3-gowiki/src/app/wiki.go
+++ 3-gowiki/src/app/wiki.go
@@ -1,27 +1,39 @@
 package main
 
 import (
-	"page"
+    "page"
+    "regexp"
+    "errors"
 	"log"
     "net/http"
 	"html/template"
 )
 
-func renderTemplate(w http.ResponseWriter, tmpl string, p *page.Page) {
-    t, _ := template.ParseFiles(tmpl + ".html")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("Invalid Page Title")
     }
-    err = t.Execute(w, p)
+    return m[2], nil // The title is the second subexpression.
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *page.Page) {
+    err := templates.ExecuteTemplate(w, tmpl+".html", p)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
-    p, _ := page.LoadPage(title)
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
+    p, err := page.LoadPage(title)
     if err != nil {
         http.Redirect(w, r, "/edit/"+title, http.StatusFound)
         return
@@ -30,7 +42,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/edit/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := page.LoadPage(title)
     if err != nil {
         p = &page.Page{Title: title}
@@ -39,10 +54,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/save/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     body := r.FormValue("body")
     p := &page.Page{Title: title, Body: []byte(body)}
-    err := p.Save()
+    err = p.Save()
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
