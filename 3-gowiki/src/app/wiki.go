acb49246aa881959e8d383d757b4cd6277b6a016
Arthur Caetano Sabino Santos <arthur.santos@gjccorp.com.br>
2019-10-10 14:29:17 -0300

    Handling errors

--- 3-gowiki/src/app/wiki.go
+++ 3-gowiki/src/app/wiki.go
@@ -6,14 +6,26 @@ import (
     "net/http"
 	"html/template"
 )
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *page.Page) {
     t, _ := template.ParseFiles(tmpl + ".html")
-    t.Execute(w, p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    err = t.Execute(w, p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
     p, _ := page.LoadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
     renderTemplate(w, "view", p)
 }
 
@@ -30,7 +42,11 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/save/"):]
     body := r.FormValue("body")
     p := &page.Page{Title: title, Body: []byte(body)}
-    p.Save()
+    err := p.Save()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
     http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
